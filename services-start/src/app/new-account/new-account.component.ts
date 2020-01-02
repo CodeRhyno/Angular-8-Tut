d1fc455458c3b02c58b720a3131f4442b27d7f74
SHLOK WALIA <virgoshlok@gmail.com>
2020-01-03 01:15:37 +0530

    lec112

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -7,15 +7,15 @@ import { AccountsService } from '../accounts.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService, AccountsService]
+  // providers: [LoggingService]
 })
 export class NewAccountComponent {
-  
+
   constructor(private loggingService: LoggingService,
               private accountsService : AccountsService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.loggingService.logStatusChange(accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
     this.accountsService.addAccount(accountName, accountStatus);
   }
 }
