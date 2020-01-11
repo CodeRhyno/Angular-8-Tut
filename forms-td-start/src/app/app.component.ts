2013ff382182a866690111a33f862c7110f4350d
SHLOK WALIA <virgoshlok@gmail.com>
2020-01-11 12:43:02 +0530

    Lec194

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('formElement', {static: true}) signUpForm: NgForm;
+  questions: {
+    val: string,
+    question: string
+  }[] = [
+    {
+      val: 'pet',
+      question: 'Name of your pet?'
+    },
+    {
+      val: 'teacher',
+      question: 'Name of your teacher?'
+    },
+    {
+      val: 'friend',
+      question: 'Name of your friend?'
+    }
+  ]
+  selectedQuestion: string;
+  answer: string = '';
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
   }
 }
