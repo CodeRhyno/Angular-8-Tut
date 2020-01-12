363fe21b7cc1cfaef4ae50610b4417c405ce2ade
SHLOK WALIA <virgoshlok@gmail.com>
2020-01-12 11:10:56 +0530

    Assignment6

--- forms-td-start/src/app/signup-form/signup-form.component.ts
+++ forms-td-start/src/app/signup-form/signup-form.component.ts
@@ -10,6 +10,7 @@ export class SignupFormComponent implements OnInit {
   @ViewChild('formElement', {static: true}) signupForm: NgForm;
   formSubmitted = false;
   suggestedEmails = ['a@d.com', 't@e.com', 'y@f.com', 'u@n.com'];
+  suggestedEmail: string;
   subscriptions = [
     {
       id: 1,
@@ -31,7 +32,15 @@ export class SignupFormComponent implements OnInit {
   }
 
   suggestEmail() {
-
+    this.suggestedEmail = this.suggestedEmails[Math.floor(Math.random() * 4)];
+    console.log("dkjfbvdfjkv");
+    this.signupForm.form.patchValue(
+      {
+        userData: {
+          email: this.suggestedEmail
+        }
+      }
+    );
   }
 
   submitForm(form: NgForm) {
